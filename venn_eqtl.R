e4fdd5c35d6e2bec5f688f0d54cc3e7a83bf5aea
chenni7 <140827500+chenni7@users.noreply.github.com>
2025-09-27 00:05:48 +0800

    Add files via upload

---
+++ venn_eqtl.R
@@ -0,0 +1,26 @@
+
+library(VennDiagram)
+
+setwd("C:/Users/Lenovo/Desktop/棕榈化代码/palmitoylation/1_get_palmitoylation_gene")
+
+data1=read.table("eQTL.txt",header=F,sep = "\t")
+dfset1=as.vector(data1[,1])
+data2=read.table("palmitoylation.txt",header=F,sep = "\t")
+dfset2=as.vector(data2[,1])
+
+
+venn.plot <- venn.diagram(
+  x = list(Set1 = dfset1, Set2 = dfset2),
+  category.names = c("eQTL", "Palmitoylation"),
+  fill = c("skyblue", "pink"),cat.pos=c(0,0),
+  alpha = 0.5,cex = 1.5,
+  cat.cex = 1.5,ext.dist = -0.1,
+  ext.length = 0.85,
+  filename = NULL
+)
+pdf("venn_eqtl.pdf",12,12);
+grid.draw(venn.plot);
+dev.off()
+mylist= list(Set1 = dfset1, Set2 = dfset2)
+vedata=Reduce(intersect,mylist)
+write.table(vedata,"Venn_eqtl.txt",sep="\t",quote=F,col.names=F,row.names=F)
